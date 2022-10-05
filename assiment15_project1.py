11ee7096487b768db208f83d363f48b3749bd69f
vishurashu <112474600+vishurashu@users.noreply.github.com>
2022-10-05 10:18:12 +0530

    Add files via upload

---
+++ assiment15_project1.py
@@ -0,0 +1,6 @@
+#Write a python script to create a String in 3 different possible ways
+x=input(" enter your string  ")
+print(x)
+a="123"
+str="a"
+print(type(str))
---
+++ assingment15_project7.py
@@ -0,0 +1,8 @@
+#Write a python script to determine whether a string contains a specific substring.
+str_1="how are you!!"
+str_2="you!!"
+if str_2 in str_1:
+    print("found")
+else:
+    print("not! found...")
+print()        
